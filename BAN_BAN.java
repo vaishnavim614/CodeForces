1c92fa424e903dcb1b45f3ae3c59b7e6ede95ec0
vaishnavim614 <321vaishnavi0043@dbit.in>
2024-03-19 22:50:31 +0530

    committed

---
+++ BAN_BAN.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BAN_BAN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int l=1,r=3*n-1;
+
+        }
+    }
+}
